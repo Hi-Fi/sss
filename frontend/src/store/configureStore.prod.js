019fea2cd939480a24528aa67da2d74f69838c99
Juho Saarinen <juho.saarinen@gmail.com>
2025-06-26 22:00:34 +0300

    chore: move to redux-toolkit

--- frontend/src/store/configureStore.prod.js
+++ frontend/src/store/configureStore.prod.js
@@ -1,12 +1,14 @@
-import { createStore, applyMiddleware, compose } from 'redux';
+import { configureStore as toolkitConfigureStore } from '@reduxjs/toolkit'
 import rootReducer from '../reducers';
 import promise from 'redux-promise';
-
-// Middleware you want to use in production:
-const enhancer = applyMiddleware(promise);
 
 export default function configureStore(initialState) {
   // Note: only Redux >= 3.1.0 supports passing enhancer as third argument.
   // See https://github.com/rackt/redux/releases/tag/v3.1.0
-  return createStore(rootReducer, initialState, enhancer);
+  return toolkitConfigureStore({
+    reducer: rootReducer,
+    devTools: false,
+    preloadedState: initialState,
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(promise),
+  });
 };
